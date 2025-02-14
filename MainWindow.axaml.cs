e39994cac1fa818f4b217adb4e0c96960527c538
Brian Jackson <49730560+rhunyc@users.noreply.github.com>
2025-02-14 13:05:59 -0600

    in progress

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,16 +1,26 @@
 using Avalonia.Controls;
-using LibUsbDotNet.Info;
-using LibUsbDotNet.Main;
-using LibUsbDotNet;
 using System.Collections.ObjectModel;
 using System;
 using System.Collections.Generic;
-
+using Avalonia.Input;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using PCSC.Exceptions;
+using Tmds.DBus.Protocol;
+using PCSC;
+using PCSC.Exceptions;
+using PCSC.Utils;
+using PCSC.Monitoring;
 
 namespace AvaloniaApplication1
 {
+    
+
     public partial class MainWindow : Window
     {
+        public IDeviceMonitor? Monitor { get; set; } = null;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -19,28 +29,99 @@ namespace AvaloniaApplication1
             int vendorId = 0x072F;  // ACS Vendor ID
             int productId = 0x2200; // ACR122U Product ID
 
-            var devices = UsbDevice.AllDevices;
+            //ACRReader = UsbDevice.OpenUsbDevice(new UsbDeviceFinder(vendorId, productId));
+            //lblInfo.Content = ACRReader == null ? "ACR112U not found." : "ACR122U found!";
+            var factory = DeviceMonitorFactory.Instance;
+            Monitor = factory.Create(SCardScope.System);
 
-            if (devices.Count > 0)
-            {
-                //
-            }
+            Monitor.Initialized += OnInitialized;
+            Monitor.StatusChanged += OnStatusChanged;
+            Monitor.MonitorException += OnMonitorException;
 
-            UsbDevice device = UsbDevice.OpenUsbDevice(new UsbDeviceFinder(vendorId, productId));
+            Monitor.Start();
 
-            if (device == null)
-            {
-                Console.WriteLine("ACR122U not found.");
-                return;
-            }
 
-            lblInfo.Content="ACR122U found!";
 
-            
+            Task.Run(() => {
+                
+                //PollForNFCTag();
+            });
 
-            device.Close();
+
+
         }
 
-        public static UsbDevice MyUsbDevice;
+        private void Monitor_StatusChanged(object sender, StatusChangeEventArgs e)
+        {
+            Console.Write("Event");
+        }
+
+        static void PollForNFCTag()
+        {
+            var contextFactory = ContextFactory.Instance;
+            using (var context = contextFactory.Establish(SCardScope.System))
+            {
+                try
+                {
+                    // Get available readers
+                    var readers = context.GetReaders();
+
+                    if (readers.Length == 0)
+                    {
+                        Console.WriteLine("No readers found.");
+                        return;
+                    }
+
+                    Console.WriteLine("Available readers:");
+                    foreach (var reader in readers)
+                    {
+                        Console.WriteLine(reader);
+                    }
+
+                    // Use the first available reader
+                    string readerName = readers[0];
+
+
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"PCSC Exception: {ex.Message}");
+                }
+            }
+        }
+
+        private static void WaitUntilSpaceBarPressed()
+        {
+            while (Console.ReadKey().Key != ConsoleKey.Spacebar) { }
+        }
+
+        private static void OnMonitorException(object sender, DeviceMonitorExceptionEventArgs args)
+        {
+            Console.WriteLine($"Exception: {args.Exception}");
+        }
+
+        private static void OnStatusChanged(object sender, DeviceChangeEventArgs e)
+        {
+            foreach (var removed in e.DetachedReaders)
+            {
+                Console.WriteLine($"Reader detached: {removed}");
+            }
+
+            foreach (var added in e.AttachedReaders)
+            {
+                Console.WriteLine($"New reader attached: {added}");
+            }
+        }
+
+        private static void OnInitialized(object sender, DeviceChangeEventArgs e)
+        {
+            Console.WriteLine("Current connected readers:");
+            foreach (var name in e.AllReaders)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
     }
 }
--- obj/Debug/net8.0/AvaloniaApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/AvaloniaApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ded490d980a706b22d27b39a437ebd967c7fe388")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5664cec41fbb54138d33da3b457ec6d25c0b849")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
